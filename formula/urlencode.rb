c723d879aa9922c5f41fbf61c88cb3b02b23b29e
Brian W. Wolter <brian.wolter@bounceexchange.com>
2019-02-26 15:09:55 -0500

    Version 1.1

--- formula/urlencode.rb
+++ formula/urlencode.rb
@@ -1,13 +1,12 @@
 
 class Urlencode < Formula
   homepage "https://github.com/bww/urlencode"
-  url "https://github.com/bww/urlencode/releases/download/v1.0/urlencode-v1.0-darwin-amd64.tgz"
-  sha256 "4fcf5164fc01b85ac42b10f783cf59fa5344ee4bec84aed3737671e84ab62eff"
-  version "v1.0"
+  url "https://github.com/bww/urlencode/releases/download/v1.1/urlencode-v1.1-darwin-amd64.tgz"
+  sha256 "f6fb5d69af1c11c00aba3e4ef0093e8e522361ba438318476d904308e80efd9d"
+  version "v1.1"
   
   def install
     system "install", "-d", "#{bin}"
     system "install", "-m", "0755", "bin/urlenc", "#{bin}/urlenc"
-    system "install", "-m", "0755", "bin/urldec", "#{bin}/urldec"
   end
 end
