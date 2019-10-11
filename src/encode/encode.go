18e322140c04aec98ed842a5b4811bfc5cd4c4a4
Brian W. Wolter <brian.wolter@bounceexchange.com>
2019-10-11 13:24:53 -0400

    Revamp

--- src/encode/encode.go
+++ src/encode/encode.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"net/url"
 	"os"
+	"path"
 	"strings"
 )
 
@@ -14,60 +15,152 @@ var ( // set at compile time via the linker
 	githash = "000000"
 )
 
+var verbose bool
+var exe string
+
+func init() {
+	exe = path.Base(os.Args[0])
+}
+
 func main() {
-	var err error
+	err := cmd()
+	if err != nil {
+		fmt.Println("***", err)
+		os.Exit(1)
+	}
+}
+
+func cmd() error {
+	cmdline := flag.NewFlagSet(os.Args[0], flag.ExitOnError)
+	var (
+		fVerbose = cmdline.Bool("verbose", false, "Be more verbose.")
+	)
+	cmdline.Parse(os.Args[1:])
+
+	verbose = *fVerbose
+
+	args := cmdline.Args()
+	if len(args) < 1 {
+		return help()
+	}
+
+	switch c := args[0]; c {
+	case "enc":
+		return encode(args[1:])
+	case "dec":
+		return decode(args[1:])
+	case "list":
+		return list(args[1:])
+	case "version":
+		return info()
+	case "help":
+		return help()
+	default:
+		return fmt.Errorf("Unsupported command: %v;\n\ttry: %v help", c, exe)
+	}
+}
+
+func encode(args []string) error {
+	data, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		return err
+	}
+
+	v := url.QueryEscape(strings.TrimSpace(string(data)))
+	fmt.Println(v)
+
+	return nil
+}
+
+func decode(args []string) error {
 	var extractKeys flagList
 
 	cmdline := flag.NewFlagSet(os.Args[0], flag.ExitOnError)
-	var (
-		fEncode  = cmdline.Bool("enc", false, "URLencode the input.")
-		fDecode  = cmdline.Bool("dec", false, "URLdecode the input.")
-		fVerbose = cmdline.Bool("verbose", false, "Be more verbose.")
-		fVersion = cmdline.Bool("version", false, "Display the version.")
-	)
 	cmdline.Var(&extractKeys, "key", "Extract the value associated with the provided key. Provide -key repeatedly to extract multiple values.")
 	cmdline.Parse(os.Args[1:])
 
-	if *fVersion {
-		if version == githash {
-			fmt.Println(version)
-		} else {
-			fmt.Printf("%s (%s)\n", version, githash)
-		}
-		return
-	}
-
 	data, err := ioutil.ReadAll(os.Stdin)
 	if err != nil {
-		panic(err)
+		return err
 	}
 
-	v := strings.TrimSpace(string(data))
-	if *fEncode {
-		v = url.QueryEscape(v)
-	} else if *fDecode {
-		v, err = url.QueryUnescape(v)
-		if err != nil {
-			fmt.Printf("Invalid: %v\n", err)
-		}
+	v, err := url.QueryUnescape(strings.TrimSpace(string(data)))
+	if err != nil {
+		return err
 	}
 
 	if len(extractKeys) < 1 {
 		fmt.Println(v)
-		return
+		return nil
 	}
 
 	u, err := url.ParseQuery(v)
 	if err != nil {
-		fmt.Printf("Invalid: %v\n", err)
+		return err
 	}
+
 	for _, e := range extractKeys {
-		if *fVerbose {
+		if verbose {
 			fmt.Printf("%s: %s\n", e, u.Get(e))
 		} else {
 			fmt.Println(u.Get(e))
 		}
 	}
+
+	return nil
+}
+
+func list(args []string) error {
+	data, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		return err
+	}
+
+	u, err := url.ParseQuery(strings.TrimSpace(string(data)))
+	if err != nil {
+		return err
+	}
+
+	var w int
+	var wf string
+	for k, _ := range u {
+		if l := len(k); l > w {
+			w = l
+		}
+	}
+	wf = fmt.Sprintf("%%%ds", w)
+
+	for k, v := range u {
+		fmt.Printf(wf+": %s\n", k, strings.Join(v, ", "))
+	}
+
+	return nil
+}
+
+func info() error {
+	if version == githash {
+		fmt.Println(version)
+	} else {
+		fmt.Printf("%s (%s)\n", version, githash)
+	}
+	return nil
+}
+
+func help() error {
+	fmt.Printf(`%s [global options] <command> [options]
+
+Commands:
+    enc        Encode URL parameters
+    dec        Decode URL parameters
+    list       Parse and list URL parameters
+    version    Display version information
+    help       Display this help information
+
+For help on a command, use:
+    $ %s <command> -h
+
+`, exe, exe)
+	return nil
 }
 
 type flagList []string
